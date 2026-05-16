202e04367dcf33d293311d5c3c7d3db0b6ed7fc8
Ramses <mynamerabbak@proton.me>
2026-05-16 16:33:56 +0800

    Update multilingual Woujouh QR pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  i18n: {
+    locales: ['jp', 'fr', 'en', 'ar'],
+    defaultLocale: 'jp',
+    routing: "manual"
+  },
   output: "static"
 });
